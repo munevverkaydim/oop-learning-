cec5c6add1d1a78027445f13eeffb68562bf3620
Münevver Kaydım <kaydimmunevver@gmail.com>
2026-05-06 16:41:42 +0300

    feat: add BMI calculator in Java
    - Calculates BMI using weight and height from the user.
    - Uses Math.pow for the area calculation (height squared).
    - Uses double data type for a correct result.
    - Added scanner.close() to clean up the code.

---
+++ projects/BMICalculator.java
@@ -0,0 +1,27 @@
+package projects;
+
+import java.util.Scanner;
+
+public class BMICalculator {
+    public static void main(String[] args){
+        //BMI (beden kitle indeksi) = ağırlık / (boy * boy)
+        //ağırlık: kg cinsi
+        //boy: m cinsi
+       
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("lütfen boyunuzu giriniz:");
+        float boy = scanner.nextFloat();
+
+        System.out.println("lütfen kilonuzu girin:");
+        float kilo = scanner.nextFloat();
+
+        double BMI = kilo / Math.pow(boy, 2);  //Math.pow fonksiyonu her zaman double değer döndürür.
+
+        System.out.println("BMI:" + BMI);
+
+
+        scanner.close(); // bu komutla scanner ı kapatmak iyi bir mühendislik alışkanlığıdır. Bu, sistem kaynaklarının gereksiz yere meşgul edilmesini önler.
+    }
+    
+}
