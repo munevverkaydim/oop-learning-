4b59a72822f7aa6fe630b90b836a2c1488527ea5
Münevver Kaydım <kaydimmunevver@gmail.com>
2026-05-07 16:20:07 +0300

    feat: add boolean and logic operators in Java
    - Shows how to use comparison operators like > and ==.
    - Shows logical AND (&&) and OR (||) operators.
    - Added examples for true and false results.

---
+++ LessonsWithYT/BooleanLogicBasics.java
@@ -0,0 +1,29 @@
+package LessonsWithYT;
+
+public class BooleanLogicBasics {
+    public static void main(String[] args){
+        
+        //  > , < , <= , >=  
+        
+        boolean sonuc;
+        sonuc = 10 > 5;
+        System.out.println(sonuc);
+
+        boolean sonuc2;
+        sonuc2 = 5 > 5;
+        System.out.println(sonuc2);
+
+        //  == , != 
+        //  &&  || 
+
+        boolean cevap;
+        cevap = (5 == 5) && (7 != 8);
+        System.out.println(cevap);
+
+        boolean cevap2;
+        cevap2 = (5 == 5) || (7 != 8);
+        System.out.println(cevap2);
+
+    }
+    
+}
