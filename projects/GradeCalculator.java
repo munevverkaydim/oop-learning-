e7c68fd0f0146bc46708291c35e74b0b5edfd4d5
Münevver Kaydım <kaydimmunevver@gmail.com>
2026-05-07 17:13:37 +0300

    feat: add student grade calculator
    - Created a program to calculate grades from 0 to 100.
    - Used if, else if, and else for different score ranges.
    - Practice with logical AND (&&) to check intervals.
    - Prints grade results like 'zayif', 'gecer', or 'pekiyi'.

---
+++ projects/GradeCalculator.java
@@ -0,0 +1,42 @@
+package projects;
+
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args){
+
+        /* karne not uygulaması
+        0 - 44: zayıf
+        45 - 54: geçer
+        55 - 64: orta
+        65 - 74: iyi
+        75 - 84: çokiyi
+        85 - 100: pekiyi  
+         */
+
+        System.out.print("notunuzu giriniz:");
+        Scanner scanner = new Scanner(System.in);
+        int not = scanner.nextInt();
+
+        if ((not >= 0) && (not <= 44)){
+            System.out.println("zayif");
+
+        }else if((not >= 45) && (not <= 54)){
+            System.out.println("gecer");
+
+        }else if((not >= 55) && (not <= 64)){
+            System.out.println("orta");
+
+        }else if((not >= 65) && (not <= 74)){
+            System.out.println("iyi");
+
+        }else if((not >= 75) && (not <= 84)){
+            System.out.println("cokiyi");
+
+        }else{
+            System.out.println("pekiyi");
+        }
+
+    }
+    
+}
