021ed8305fcb51d2547f226d7c627f9640c441cd
Münevver Kaydım <kaydimmunevver@gmail.com>
2026-05-08 15:44:50 +0300

    feat: Implement Method Overloading in Java
    - Create multiple methods with the same name but different parameters.
    - Demonstrate how Java chooses the correct method based on arguments.
    - Show flexibility with different data types (int and String).
    - Practice method signatures and parameter variations.

---
+++ LessonsWithYT/Lesson14_MethodOverLoading.java
@@ -0,0 +1,24 @@
+package LessonsWithYT;
+
+public class Lesson14_MethodOverLoading {
+    // Method Overloading !! ÖNEMLİİİ
+    // Aynı fonksiyonu birden fazla kez yazıp içine aldığı parametreleri değiştirmek
+
+    public static void showYourMessage(int sayi1, int sayi2, String metin){ //void değer döndürmeyecek demek
+        System.out.println("merhaba " + sayi1 + " " + sayi2 + " " + metin);
+    }
+
+    public static void showYourMessage(int sayi1, String metin){ 
+        System.out.println("merhaba " + sayi1 + " " + metin);
+    }
+
+    public static void showYourMessage(int sayi1, int sayi2){ 
+        System.out.println("merhaba " + sayi1 + " " + sayi2);
+    }
+
+    public static void main(String[] args){
+        showYourMessage(3, 2);
+        showYourMessage(2, 6, "World");
+        showYourMessage(9, "Java");
+    }
+}
