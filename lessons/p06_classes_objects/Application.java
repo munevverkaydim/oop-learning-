31e546718da861119a929808b1eda76f25082bd1
Münevver Kaydım <kaydimmunevver@gmail.com>
2026-05-11 21:44:02 +0300

    feat: introduce basic class structure and encapsulation
    - Define a Car class with public and private fields.
    - Understand visibility using public and private modifiers.
    - Create multiple objects (car1, car2) from the Car class.
    - Implement Getter methods to access private attributes safely.
    - Use 'this' keyword to reference class members.

---
+++ lessons/p06_classes_objects/Application.java
@@ -0,0 +1,17 @@
+package lessons.p06_classes_objects;
+
+public class Application {
+    public static void main(String[] args) {
+        Car car1 = new Car(); //burda car1 objesi oluşturduk. Car methodunu kullanarak.
+        car1.colour = "green"; //colour ı public yaptığımız için başka class lardan erişebiliyoruz. brand e direkt erişemeyiz
+        car1.maxSpeed = 160;
+
+        System.out.println(car1.colour + " " + car1.maxSpeed);
+
+        Car car2 = new Car(); //burda car2 objesi oluşturduk.
+        car2.colour = "red";
+        car2.maxSpeed = 200;
+
+        System.out.println(car2.colour + " " + car2.maxSpeed);        
+    }
+}
