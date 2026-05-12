de9af981fa2de79f84c8236594d24614c79d11de
Münevver Kaydım <kaydimmunevver@gmail.com>
2026-05-12 14:51:06 +0300

    feat: implement encapsulation with House class example
    Create Hause class with private fields for data security.
    
    Add getter and setter methods to control access to class properties.
    
    Implement Application class to demonstrate object instantiation and method usage.
    
    Use this keyword to reference class-level variables.

---
+++ lessons/p06_encapsulation/Hause.java
@@ -0,0 +1,40 @@
+package lessons.p06_encapsulation;
+
+public class Hause {
+    //Bir sınıf bir taslaktır (ev planı gibi)
+    //Constructor, o evi inşa ederken eline tuğlayı, çimentoyu aldığın ve evin rengine, oda sayısına karar verdiğin o "yapım aşaması"dır.
+    private String colour;
+    private int howManyRooms;
+    private boolean hasGarage; 
+
+
+    //her class ın kendine ait methodu (function bulunur)
+    public String getColour(){ //GETTER
+        return this.colour; ////this burda bu classın demek. 
+        //Bu içinde bulundugun sınıfın kendi özelliklerini aynı sınıf içinde kullanırken "this".
+    }
+    public void setColour(String clr){//SETTER
+        this.colour = clr;
+    }
+
+
+    public int getHowManyRooms(){ //public yapıyoruz ki bu House class ı dışında kullanabilelim bu method u. 
+        return this.howManyRooms;
+    }
+    public void setHowManyRooms(int hwMR){
+        this.howManyRooms = hwMR;
+    }
+
+    public boolean gethasGarage(){ //get method u değer okuyor. yani değer döndürmek zorunda.
+        return this.hasGarage;
+    }
+    public void sethasGarage(boolean hasG){ //set methodu değer döndürmüyor(void)
+        this.hasGarage =hasG;
+    }
+}
+
+//ENCAPSULATİON!!: Class ın değişebilen özelliklerini başka class lardan değiştirmeyin, okumayın!!
+// method ile değiştir, oku. --> setter, getter methodları ile.
+
+
+//kalabalık ekiplerle çalışırken method u kim çağırdı görebiliriz.
