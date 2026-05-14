3ae1f855425d9aa6c8c2cb743e3d52074c08767b
Münevver Kaydım <kaydimmunevver@gmail.com>
2026-05-14 16:46:30 +0300

    feat: create Helicopter class with composition
    - Add Engine, Rotor, and Store as parts of the Helicopter.
    - Add a constructor to set helicopter parts.
    - Add a default constructor to create a standard helicopter.
    - Add getter and setter methods to access variables.
    - Add Application class to run and test the code.

--- lessons/p10_composition/Helicopter.java
+++ lessons/p10_composition/Helicopter.java
@@ -1,11 +1,52 @@
 package lessons.p10_composition;
 
 public class Helicopter {
-
+    // Parçaları nesne olarak tanımlıyoruz (Composition)
     private int tailNumber;
     private Engine engine; //composition bu. oop nin özelliği bu. nesne içinde nesne tutulabiliyor
     private Rotor rotor; //temel veri tiplerinden farklı veri tipi yerine nesne yazdık.
     private Store store;
 
+    
+    //constructor1
+    public Helicopter(int tailNumber, Engine engine, Rotor rotor, Store store){
+        this.tailNumber = tailNumber;
+        this.engine = engine;
+        this.rotor = rotor;
+        this.store = store;
+    }
+    //constructor2
+    public Helicopter(){//burda constructor içinde obje ürettik.(composition yani nesne içinde nesne)
+        this(1234, new Engine(), new Rotor(), new Store());
+    }
 
+
+    //setter getter methodları:
+    public void setTailNumber(int tnum){
+        this.tailNumber = tnum;
+    }
+    public int getTailNumber(){
+        return this.tailNumber;
+    }
+
+    public void setEngine(Engine eng){
+        this.engine = eng;
+    }
+    public Engine getEngine(){
+        return this.engine;
+    }
+
+    public void setRotor(Rotor rt){
+        this.rotor = rt;
+    }
+    public Rotor getRotor(){
+        return this.rotor;
+    }
+
+    public void setStore(Store st){
+        this.store = st;
+    }   
+    public Store getStore(){
+        return this.store;
+    }
 }
