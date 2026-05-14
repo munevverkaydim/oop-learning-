7443a39d5ccad5df079d6f03cca684205263c928
Münevver Kaydım <kaydimmunevver@gmail.com>
2026-05-14 19:00:54 +0300

    feat: add Bird class with method override
    - Create Bird class as a child of Animal.
    - Use super(name) to send the name to the parent class.
    - Override the makeNoise method for a specific bird sound.
    - Keep the code simple without extra variables.

--- lessons/p11_polymorphism/Bird.java
+++ lessons/p11_polymorphism/Bird.java
@@ -1,5 +1,19 @@
 package lessons.p11_polymorphism;
 
-public class Bird {
+public class Bird extends Animal{
 
+    //burada variable tanımlamayalım
+
+    //constructor
+    public Bird(String name){
+        super(name);
+    }
+
+    //businnes logic
+    @Override
+    public void makeNoise(){
+        System.out.println("bird is making sound. cik cik");
+    }
+
+    //getter setter tanımlamıyoruz çünkü paramater yok bird class ında.
 }
