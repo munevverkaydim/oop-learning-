7ee749d66e7142ed3decbee9779de95768802cdc
Münevver Kaydım <kaydimmunevver@gmail.com>
2026-05-16 20:37:13 +0300

    feat: implement Square class
    - Add side field and constructor
    - Implement area and perimeter calculation for square
    - Add getter and setter for side

---
+++ lessons/p13_abstraction/Square.java
@@ -0,0 +1,34 @@
+package lessons.p13_abstraction;
+
+public class Square extends Shape {
+    //parameter
+    double side;
+
+    //constructor
+    public Square(String name, double side){
+        super(name);
+        this.side = side;
+    }
+
+    //business logic
+    @Override
+    public double findArea(){
+        double area = this.side * this.side;
+        return area;
+    }
+
+    @Override
+    public double findPerimeter(){
+        double perimeter = this.side * 4;
+        return perimeter;
+    }
+
+    //getter setter
+    public void setSide(double side){
+        this.side = side;
+    }
+    public double getSide(){
+        return this.side;
+    }
+
+}
