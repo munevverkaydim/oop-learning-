f967146262fe93caa2efd7d42e230a8fdaecad47
Münevver Kaydım <kaydimmunevver@gmail.com>
2026-05-18 23:09:18 +0300

    feat: add p14_static package with Employee and Application
    - Create Employee class with static counter variables
    - Create Application class to test and demonstrate static behavior

---
+++ lessons/p14_static/Application.java
@@ -0,0 +1,21 @@
+package lessons.p14_static;
+
+public class Application {
+
+    public static void main(String[] args) {
+        
+        Employee employee1 = new Employee("Ali");
+        System.out.println(employee1.getName());
+
+        Employee employee2 = new Employee("Veli");
+        System.out.println(employee2.getName());
+
+        System.out.println(Employee.showNumOfEmployee());
+
+
+        Employee employee3 = new Employee("Ayşe");
+        System.out.println(employee3.getName());
+
+        System.out.println(Employee.showNumOfEmployee());
+    }
+}
