8b29456575fc7c1d599bbcc4e6494a9d07b00b1d
Münevver Kaydım <kaydimmunevver@gmail.com>
2026-05-18 23:08:04 +0300

    feat: add p14_static package
    - Create Employee class to demonstrate static keyword
    - Implement static variable numOfEmployee to count instances
    - Implement static method showNumOfEmployee to access class-level data

---
+++ lessons/p14_static/Employee.java
@@ -0,0 +1,30 @@
+package lessons.p14_static;
+
+public class Employee {
+
+    private String name;
+    private static int numOfEmployee = 0; // main methodu çalışmadan önce derelyicistatic değişkeb var mı diye bakar.
+    // static değişken görünce bellekte yer açılır daha program başlamadan. 
+
+    //constructor
+    public Employee(String name){
+        this.name = name;
+        numOfEmployee ++;
+    }
+
+
+    //getter setter
+    public void setName(String nme){
+        this.name = nme;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public static int showNumOfEmployee(){
+        return numOfEmployee;
+    }
+}
+
+//static method lar da değişkenler de class a özgü. kullanıcının bunlara erişmesi tercih edilmez.
+//static değişkenlerin ve method ların nesnelerle bir alakası yok.
